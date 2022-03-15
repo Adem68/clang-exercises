e12f09e7a923b3c87408ca29c1baf3cb95634cce
Adem Özcan <info@ademfurkan.com>
2022-03-15 10:39:16 +0300

    add first exercise of today

---
+++ 15-03-2022/first-exercise.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    printf("Enter two integers, and I will tell you\n");
+    printf("the relationships they satisfy: ");
+
+    int num1;
+    int num2;
+    
+    scanf("%d %d", &num1, &num2);
+    
+    if (num1 == num2) {
+        printf("%d is equal to %d\n", num1, num2);
+    }
+    
+    if (num1 != num2) {
+        printf("%d is not equal to %d\n", num1, num2);
+    }
+    
+    if (num1 < num2) {
+        printf("%d is less than %d\n", num1, num2);
+    }
+    
+    if (num1 > num2) {
+        printf("%d is greater than %d\n", num1, num2);
+    }
+    
+    if (num1 <= num2) {
+        printf("%d is less than or equal to %d\n", num1, num2);
+    }
+    
+    if (num1 >= num2) {
+        printf("%d is greater than or equal to %d\n", num1, num2);
+    }
+}
